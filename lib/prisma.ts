fb6268eeaebff320d950e7c26c001143235408c7
Adam McCombs <adamjmccombs@gmail.com>
2025-02-02 11:22:17 -0600

    env loading

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from "../generated/client/deno/edge.ts";
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { config } from "https://deno.land/std@0.163.0/dotenv/mod.ts";
-const envVars = await config();
+import "jsr:@std/dotenv/load";
 
 const prisma = new PrismaClient({
     datasources: {
         db: {
-            url: envVars.DATABASE_URL,
+            url: Deno.env.get("DATABASE_URL"),
         },
     },
 }).$extends(withAccelerate());
